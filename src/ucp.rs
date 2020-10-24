f4336ee5d62d3228afb51371b4fb16ff4761d8e1
Runji Wang <wangrunji0408@163.com>
2020-10-24 16:35:38 +0800

    add UCP tag vectored API

--- src/ucp.rs
+++ src/ucp.rs
@@ -5,6 +5,7 @@ use futures::task::AtomicWaker;
 use std::collections::VecDeque;
 use std::ffi::CString;
 use std::future::Future;
+use std::io::{IoSlice, IoSliceMut};
 use std::mem::{ManuallyDrop, MaybeUninit};
 use std::net::SocketAddr;
 use std::os::raw::c_void;
@@ -266,10 +267,47 @@ impl Worker {
                 Some(callback),
             )
         };
-        if status.is_null() {
-            trace!("tag_recv: complete.");
-            RequestHandle::Ready(0)
-        } else if UCS_PTR_IS_PTR(status) {
+        if UCS_PTR_IS_PTR(status) {
+            RequestHandle::from(status, 0)
+        } else {
+            panic!("failed to recv tag: {:?}", UCS_PTR_RAW_STATUS(status));
+        }
+    }
+
+    pub fn tag_recv_vectored(&self, tag: u64, iov: &mut [IoSliceMut<'_>]) -> RequestHandle {
+        trace!(
+            "tag_recv_vectored: worker={:?} iov.len={}",
+            self.handle,
+            iov.len()
+        );
+        unsafe extern "C" fn callback(
+            request: *mut c_void,
+            status: ucs_status_t,
+            info: *mut ucp_tag_recv_info,
+        ) {
+            let length = (*info).length;
+            trace!(
+                "tag_recv_vectored: complete. req={:?}, status={:?}, len={}",
+                request,
+                status,
+                length
+            );
+            let request = &mut *(request as *mut Request);
+            request.length = length as usize;
+            request.waker.wake();
+        }
+        let status = unsafe {
+            ucp_tag_recv_nb(
+                self.handle,
+                iov.as_ptr() as _,
+                iov.len() as _,
+                ucp_dt_type::UCP_DATATYPE_IOV as _,
+                tag,
+                u64::max_value(),
+                Some(callback),
+            )
+        };
+        if UCS_PTR_IS_PTR(status) {
             RequestHandle::from(status, 0)
         } else {
             panic!("failed to recv tag: {:?}", UCS_PTR_RAW_STATUS(status));
@@ -530,6 +568,42 @@ impl Endpoint {
             RequestHandle::Ready(buf.len())
         } else if UCS_PTR_IS_PTR(status) {
             RequestHandle::from(status, buf.len())
+        } else {
+            panic!("failed to send tag: {:?}", UCS_PTR_RAW_STATUS(status));
+        }
+    }
+
+    pub fn tag_send_vectored(&self, tag: u64, iov: &[IoSlice<'_>]) -> RequestHandle {
+        trace!(
+            "tag_send_vectored: endpoint={:?} iov.len={}",
+            self.handle,
+            iov.len()
+        );
+        unsafe extern "C" fn callback(request: *mut c_void, status: ucs_status_t) {
+            trace!(
+                "tag_send_vectored: complete. req={:?}, status={:?}",
+                request,
+                status
+            );
+            let request = &mut *(request as *mut Request);
+            request.waker.wake();
+        }
+        let status = unsafe {
+            ucp_tag_send_nb(
+                self.handle,
+                iov.as_ptr() as _,
+                iov.len() as _,
+                ucp_dt_type::UCP_DATATYPE_IOV as _,
+                tag,
+                Some(callback),
+            )
+        };
+        let total_len = iov.iter().map(|v| v.len()).sum();
+        if status.is_null() {
+            trace!("tag_send_vectored: complete");
+            RequestHandle::Ready(total_len)
+        } else if UCS_PTR_IS_PTR(status) {
+            RequestHandle::from(status, total_len)
         } else {
             panic!("failed to send tag: {:?}", UCS_PTR_RAW_STATUS(status));
         }
